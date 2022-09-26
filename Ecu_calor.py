2b7bf5b3711d18981d4ea5f119899dc1a22fa278
GPeyB <89151612+GPeyB@users.noreply.github.com>
2022-09-25 20:42:35 -0500

    Update Ecu_calor.py

--- Ecu_calor.py
+++ Ecu_calor.py
@@ -4,7 +4,7 @@ import numpy as np
 def ecu_calor(areaPI, coefDT, tempiOBJ, tmpEXT, n):
     # Indexación booleana de matriz de área asociada a punto inicial
     # Tiene que ser de n*n y entrar en blanco y negro
-    area_bool = areaPI < 0.9
+    area_bool = areaPI > 0.9
 
     # Coeficiente de difusividad térmica para madera
     dt_madera = coefDT
@@ -62,18 +62,45 @@ def ecu_calor(areaPI, coefDT, tempiOBJ, tmpEXT, n):
     heat_frames = solve_heat(heat_frames, area_bool)
     # lo pasamos a grados celsius
     heat_frames -= 273.15
-    
-    return heat_frames[1]  # Ajustar
+
+    return heat_frames[0], heat_frames[3599]  # Ajustar
+
+
+def create_area_pi(k, n):  # Recibe el punto inicial k y el tamaño de los arreglos
+    area_pto_ini = np.ones(n*n)
+    area_pto_ini[k] = 0  # Asigno un 1 en punto inicial para que en ecu_calor ese 1 se convierta en 300
+
+    if k > 0:
+        area_pto_ini[k - 1] = 0
+    if k < len(area_pto_ini):
+        area_pto_ini[k + 1] = 0
+    if k >= n:
+        area_pto_ini[k - n] = 0
+    if k >= n - 1:
+        area_pto_ini[k - n - 1] = 0
+    if k >= n + 1:
+        area_pto_ini[k - n + 1] = 0
+    if k < len(area_pto_ini) - n - 1:
+        area_pto_ini[k + n - 1] = 0
+    if k < len(area_pto_ini) - n + 1:
+        area_pto_ini[k + n + 1] = 0
+    if k < len(area_pto_ini) - n:
+        area_pto_ini[k + n] = 0
+
+    mat_area_pi = np.reshape(area_pto_ini, (n, n))
+    return mat_area_pi
 
 
 def main():
     n = 10  # Largo de la matriz radio del punto inicial
-    radio_pi = np.random.randint(0, 2, size=(n, n))
+    k = 55  # Punto inicial
+    matriz_pi = create_area_pi(k, n)
     coef = 8.2e-8
     temp_fuego = 300
     temp_amb = 25
-    mat_temps = ecu_calor(radio_pi, coef, temp_fuego, temp_amb, n)
-    print(mat_temps)
+    mat_temps, mat_temps2 = ecu_calor(matriz_pi, coef, temp_fuego, temp_amb, n)
+    print('\n', mat_temps, '\n', mat_temps2)
+    # Vectorizar matriz de temperaturas
 
 if __name__ == "__main__":
     main()
